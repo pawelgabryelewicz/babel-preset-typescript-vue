3d9aa14b6ade116b3a974839746471e4bd391308
Pawel Gabryelewicz <pawelgabryelewicz@users.noreply.github.com>
2019-07-21 14:38:50 +0200

    Fixes, proper dependencies

--- src/index.js
+++ src/index.js
@@ -2,6 +2,7 @@ import { declare } from "@babel/helper-plugin-utils";
 import * as fs from "fs";
 
 import pluginTransformTypeScript from "@babel/plugin-transform-typescript";
+import presetTypeScript from "@babel/preset-typescript";
 
 export default declare(
   (api, { jsxPragma, allExtensions = false, isTSX = false }) => {
@@ -9,6 +10,9 @@ export default declare(
     api.assertVersion(7);
 
     return {
+      "presets": [
+        [presetTypeScript, { jsxPragma, allExtensions, isTSX }]
+      ],
       "overrides": [{
         "test": filePath => /\.vue$/.test(filePath) && /lang="?ts"?/.test(fs.readFileSync(filePath, {encoding: "utf8"})),
         "plugins": [
