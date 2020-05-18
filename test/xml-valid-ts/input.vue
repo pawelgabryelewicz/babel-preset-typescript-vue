ff389f09c3e14cb6963d3670099cb1902ed453c7
Pawel Gabryelewicz <pawelgabryelewicz@users.noreply.github.com>
2020-05-18 11:01:56 +0200

    Tests, native Vue file parsing (non-XML compliant should work)

---
+++ test/xml-valid-ts/input.vue
@@ -0,0 +1,11 @@
+<template>
+  <div>This is a test</div>
+  <img src="source.png" />
+</template>
+
+<script lang="ts">
+  import Vue from "vue";
+  export default Vue.extend({
+    name: "TestComponent"
+  });
+</script>
