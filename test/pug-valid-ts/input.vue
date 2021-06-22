7934d6a92f8986b93c3231be2590be6f26e72c7e
coldsewoo <coldsewoo@gmail.com>
2021-06-22 14:29:32 +0900

    2.0.13 / update dependencies

--- test/pug-valid-ts/input.vue
+++ test/pug-valid-ts/input.vue
@@ -1,10 +1,16 @@
 <template lang="pug">
-  div test
+._404.narrow_content
+  h3 Uhh, there's nothing to see here...
 </template>
 
 <script lang="ts">
 import { defineComponent } from "vue";
-export default defineComponent({
-  name: "TestComponent",
-});
+
+export default defineComponent({});
 </script>
+
+<style>
+._404 {
+  margin-top: 100px;
+}
+</style>
